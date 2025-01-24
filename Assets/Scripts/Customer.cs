ee98a002da9960852475a0971da8f5c95e9c2d1b
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2025-01-24 11:27:22 +0200

    Added stuff to basic gameplay

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -6,6 +6,12 @@ public class Customer : MonoBehaviour
     float m_TimeWaiting = 0;
     [SerializeField] float m_TimeToAnnoyed = 20f; // The amount of time until the customer leaves.
 
+
+    public Customer(BobaTea drink) {
+        m_DesiredDrink = drink;
+    }
+    private Customer() {} // Default constructor. Do not call without adding drink!!!
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    List<Customer> m_WaitingCustomers = new List<Customer>();
+    [SerializeField] int m_MaxCustomersAtOnce = 1;
+
+    float m_TimeToNextCustomer = 5f;
+
+    CustomerGenerator m_CustomerGenerator = new CustomerGenerator();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,7 +19,33 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        WaitForNextCustomer();
+    }
+
+    void WaitForNextCustomer() {
+        m_TimeToNextCustomer -= Time.deltaTime;
+
+        if (m_TimeToNextCustomer <= 0) {
+            if (m_WaitingCustomers.Count >= m_MaxCustomersAtOnce) {
+                m_TimeToNextCustomer = 2f; // Wait two seconds before checking again.
+            }
+            else {
+                // Send in a new customer!
+                Customer newCustomer = new Customer(m_CustomerGenerator.GenerateRandomDrink());
+                // TODO - PUT CUSTOMER IN SCENE
+            }
+        }
+    }
+
+    bool GiveCustomerDrink(int i_CustomerNumber, BobaTea i_GivenDrink) { // Returns true if correct drink.
+        if (m_WaitingCustomers[i_CustomerNumber].CompareToDesiredDrink(i_GivenDrink)) {
+            // TODO - Animation of customer happy and leaving!
+            m_CustomerGenerator.DrinkSuccessfulyServed();
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 
@@ -34,6 +67,14 @@ public class GameManager : MonoBehaviour
             // No foams or toppings to start
             m_AvailableFoams.Add(BobaTea.Foam.None);
             m_AvailableToppings.Add(BobaTea.Topping.None);
+        }
+
+        public void DrinkSuccessfulyServed() {
+            m_DrinksToNextComplexity--;
+
+            if (m_DrinksToNextComplexity == 0) {
+                AdvanceComplexityLevel();
+            }
         }
 
         public void AdvanceComplexityLevel() {
@@ -58,7 +99,7 @@ public class GameManager : MonoBehaviour
                     break;
 
                 default:
-                    print("WARNING! Attempted to advance Customer Complexity Level beyond what's allowed! New Complexity: " + m_CustomerRequestComplexity);
+                    print("WARNING! Attempted to advance Customer Complexity Level beyond what's coded! Final Complexity: " + m_CustomerRequestComplexity);
                     break;
             }
         }
