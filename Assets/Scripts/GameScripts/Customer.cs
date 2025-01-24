4342536e490eea9401b83c94abe6376d2cf8ffea
Elad_Fisher03 <tkgs444@gmail.com>
2025-01-24 17:56:44 +0200

    prefab

--- Assets/Scripts/GameScripts/Customer.cs
+++ Assets/Scripts/GameScripts/Customer.cs
@@ -22,6 +22,7 @@ public class Customer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
     }
 
     // Update is called once per frame
