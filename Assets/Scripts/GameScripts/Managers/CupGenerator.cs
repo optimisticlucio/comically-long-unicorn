83068b4c229747ad4bbbbbecd4031b82397b6de1
Elad_Fisher03 <tkgs444@gmail.com>
2025-01-25 14:08:09 +0200

    Adding Delay to cup Generation

--- Assets/Scripts/GameScripts/Managers/CupGenerator.cs
+++ Assets/Scripts/GameScripts/Managers/CupGenerator.cs
@@ -4,7 +4,8 @@ public class CupGenerator : MonoBehaviour
 {
     [SerializeField] public DraggableBobaTea cupPrefab;  // Assign the DraggableBobaTea prefab in the Inspector
     [SerializeField] private float spawnOffsetY = 1.0f;  // Offset above parent object
-
+    [SerializeField] private float delayBetweenCups = 1.75f;  // Delay between cup spawns
+    private float lastSpawnTime = 0.0f;
     private DraggableBobaTea currentCup;
 
     void Update()
@@ -18,12 +19,14 @@ public class CupGenerator : MonoBehaviour
         if (currentCup == null)
         {
             SpawnCup();
+
+            lastSpawnTime += Time.deltaTime;
         }
     }
 
     private void SpawnCup()
     {
-        if (cupPrefab != null)
+        if (cupPrefab != null && lastSpawnTime >= delayBetweenCups)
         {
             // Calculate spawn position (right above the parent's transform)
             Vector3 localSpawnPosition = transform.localPosition + new Vector3(0, spawnOffsetY, 0);
