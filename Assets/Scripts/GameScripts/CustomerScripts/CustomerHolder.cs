8d7abe4c150bb69c2c66ceb035b7c0b53fd4494c
Elad_Fisher03 <tkgs444@gmail.com>
2025-01-25 12:27:55 +0200

    Adding bubble to customerHolder prefab

--- Assets/Scripts/GameScripts/CustomerScripts/CustomerHolder.cs
+++ Assets/Scripts/GameScripts/CustomerScripts/CustomerHolder.cs
@@ -6,9 +6,10 @@ public class CustomerHolder : MonoBehaviour
 {
     [SerializeField] public Customer m_Customer;
     [SerializeField] public SpriteRenderer m_CustomerSpriteRenderer;
+    [SerializeField] public SpriteRenderer m_CustomerbubbleSpriteRenderer;
+    private DraggableBobaTea m_Cup = new DraggableBobaTea();
     [SerializeField] private float detectionRadius = 1.0f;
     [SerializeField] private LayerMask layerMask;
-
     [SerializeField] private AudioClip annoyedSound;  // Drag audio clip here
     [SerializeField] private AudioClip enterSound;    // Drag audio clip here
     [SerializeField] private List<AudioClip> happySounds;  // Drag happy sounds here
@@ -101,10 +102,27 @@ public class CustomerHolder : MonoBehaviour
 
         m_CustomerSpriteRenderer.sprite = sprite;
         m_CustomerSpriteRenderer.sortingLayerID = SortingLayer.NameToID("Customer");
+        
+        createBubbleForCup();
 
         PlaySound(enterSound);
 
-        m_CustomerSpriteRenderer.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        // m_CustomerSpriteRenderer.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+    }
+
+    private void createBubbleForCup()
+    {
+        print("Creating bubble for cup");
+        m_CustomerbubbleSpriteRenderer.enabled = true;
+        print("Bubble sprite renderer enabled");
+        print("m_cub.mBobatea liquid: " + m_Customer.m_DesiredDrink.m_Liquid);
+        print("m_Cup.m_BobaTea: " + m_Cup.m_BobaTea);
+        m_Cup.m_BobaTea = m_Customer.m_DesiredDrink;
+        print("Set cup's boba tea to customer's desired drink");
+        m_Cup.UpdateVisuals();
+        print("Updated visuals for cup");
+        m_Cup.SetDraggable(false);
+        print("Set cup to not draggable");
     }
 
     private bool isAnnoyedIteration()
